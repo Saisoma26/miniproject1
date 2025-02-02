e9b8788cfad8019f192ee7f35d6a3a52b321117d
Saisoma26 <saiprasannasoma26@gmail.com>
2025-02-02 00:06:18 -0500

    removed basepath

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/miniproject1">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
